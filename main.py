b26e9bdc1515dfa69481a791e8f6458b02cb5e7c
Niko919 <andreyburnosov24@gmail.com>
2023-08-04 05:01:51 +0300

    add new database

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sqlite3
 import asyncio
 import requests
 import qrcode
@@ -12,178 +11,114 @@ from tonsdk.utils import Address
 from pytonconnect import TonConnect
 
 import keyboards as kb
+import support
 from config import api_token
-from support import build_bd
 
-
-# Establishing a connection to the SQLite database named 'DB.db' and creating a cursor.
-
-con = sqlite3.connect("DB.db", check_same_thread=False)
-cur = con.cursor()
-
-build_bd(cur, con)
-
-# Creating a Telegram Bot instance and a Dispatcher object for message handling.
-
-# Creating a Bot instance using the 'api_token' provided.
+# Initialize the bot with the given API token
 bot = Bot(token=api_token)
-
-# Creating a Dispatcher instance associated with the Bot instance.
+# Create a dispatcher to handle incoming updates from the Telegram API
 dp = Dispatcher(bot)
 
-# This message handler is triggered when a user sends the '/start' command in a private chat with the bot.
-
+# Define a command handler for the '/start' command for private chats
 @dp.message_handler(commands=['start'], chat_type=types.ChatType.PRIVATE)
 async def start_command(message: types.Message):
-    # Sends a welcome message to the user, introducing the bot's purpose and functionality.
+    # Send a greeting message to the user, explaining the bot's functionality
     await message.answer("Hi👋, I am an example of a bot for checking the ownership of the NFT", reply_markup=kb.Checkkb)
+    # Further explain how the bot can help with NFT collection checking
     await message.answer("With my help, you can check if you have an NFT from the TON Footsteps collection")
 
-    # Checks if the user's Telegram ID is already present in the 'Users' database table.
-    # If not, it adds the user's ID and username to the 'Users' table as a new entry.
-    if not cur.execute(f"SELECT id_tg FROM Users WHERE id_tg == {message.from_user.id}").fetchall():
-        cur.execute(f"INSERT INTO Users (id_tg, username) VALUES ({message.from_user.id}, '{message.from_user.username}')")
-        con.commit()
-
-# A message handler function to check if the user has a footstep NFT and respond accordingly.
-
+# Define a message handler for checking the 'footstep NFT' in private chats
 @dp.message_handler(text='Check for footstep NFT', chat_type=types.ChatType.PRIVATE)
 async def connect_wallet_tonkeeper(message: types.Message):
-    # Checking if the user's wallet address is present in the database for the given Telegram ID.
-    # If the address is not available, prompt the user to connect their wallet (Tonkeeper or Tonhub).
-    if cur.execute(f"SELECT address FROM Users WHERE id_tg == {message.from_user.id}").fetchall()[0][0] is None:
-        await message.answer(text="To check for the presence of NFT, connect your wallet (Tonkeeper or Tonhub)", reply_markup=kb.Walletkb)
-    else:
-        # If the user's wallet address is available, proceed to check for the presence of the footstep NFT.
-        address = cur.execute(f"SELECT address FROM Users WHERE id_tg == {message.from_user.id}").fetchall()[0][0]
-        
-        # Forming the URL to query the TON API for the user's NFTs from the TON Footsteps collection.
-        url = f'https://tonapi.io/v2/accounts/{address}/nfts?collection=EQCV8xVdWOV23xqOyC1wAv-D_H02f7gAjPzOlNN6Nv1ksVdL&limit=1000&offset=0&indirect_ownership=false'
-        
-        try:
-            # Sending a GET request to the TON API and parsing the JSON response to extract NFT items.
-            response = requests.get(url).json()['nft_items']
-        except:
-            # If there's an error with the API request, notify the user.
-            await message.answer(text="Something went wrong...")
-            return
-        
-        # Based on the response from the TON API, informing the user about the NFT presence or absence.
-        if response:
-            await message.answer(text="You have an NFT from the TON Footsteps collection")
-        else:
-            await message.answer(text="Unfortunately, you don't have NFT from the TON Footsteps collection")
+    # Create a storage instance based on the user's ID
+    storage = support.Storage(str(message.from_user.id))
 
-
-# A message handler function to connect the user's Tonkeeper wallet to the bot.
-
-@dp.message_handler(text='Tonkeeper', chat_type=types.ChatType.PRIVATE)
-async def connect_wallet_tonkeeper(message: types.Message):
-    # Creating a TonConnect instance and restoring the connection to Tonkeeper using the provided manifest URL.
-    connector = TonConnect(manifest_url='https://raw.githubusercontent.com/AndreyBurnosov/Checking_for_nft_availability/main/pytonconnect-manifest.json')
+    # Initialize a connection using the given manifest URL and storage
+    connector = TonConnect(manifest_url='https://raw.githubusercontent.com/AndreyBurnosov/Checking_for_nft_availability/main/pytonconnect-manifest.json', storage=storage)
+    # Attempt to restore the existing connection, if any
     is_connected = await connector.restore_connection()
+
+    # If not connected, prompt the user to connect their wallet
+    if not is_connected:
+        await message.answer(text="To check for the presence of NFT, connect your wallet (Tonkeeper or Tonhub)", reply_markup=kb.Walletkb)
+        return
+
+    # Retrieve the address of the connected account
+    address = connector.account.address
+
+    # Define the URL to request NFT data specific to the user's address
+    url = f'https://tonapi.io/v2/accounts/{address}/nfts?collection=EQCV8xVdWOV23xqOyC1wAv-D_H02f7gAjPzOlNN6Nv1ksVdL&limit=1000&offset=0&indirect_ownership=false'
+
+    try:
+        # Send a GET request to the URL and parse the response as JSON
+        response = requests.get(url).json()['nft_items']
+    except:
+        # If an error occurs, inform the user and exit the function
+        await message.answer(text="Something went wrong...")
+        return
+
+    # If the response contains NFT items, inform the user that they have an NFT from the collection
+    if response:
+        await message.answer(text="You have an NFT from the TON Footsteps collection")
+    else:
+        # If no NFT items were found, inform the user that they don't have an NFT from the collection
+        await message.answer(text="Unfortunately, you don't have NFT from the TON Footsteps collection")
+
+# Define a message handler for connection to wallets (Tonkeeper or Tonhub) in private chats
+@dp.message_handler(text=['Tonkeeper', 'Tonhub'], chat_type=types.ChatType.PRIVATE)
+async def connect_wallet_tonkeeper(message: types.Message):
+    # Create a storage instance based on the user's ID
+    storage = support.Storage(str(message.from_user.id))
     
-    # Getting a list of available wallets from the TonConnect instance.
+    # Initialize a connection using the given manifest URL and storage
+    connector = TonConnect(manifest_url='https://raw.githubusercontent.com/AndreyBurnosov/Checking_for_nft_availability/main/pytonconnect-manifest.json', storage=storage)
+    # Attempt to restore the existing connection, if any
+    is_connected = await connector.restore_connection()
+
+    # If already connected, inform the user and exit the function
+    if is_connected:
+        await message.answer('Your wallet is already connected.')
+        return
+
+    # Define the connection options for different wallet
+    connection = {'Tonkeeper': 0, 'Tonhub': 2}
+
+    # Retrieve the available wallets
     wallets_list = connector.get_wallets()
 
-    # Connecting the user's Tonkeeper wallet and generating a connection URL.
-    generated_url_tonkeeper = await connector.connect(wallets_list[0])
-    
-    # Creating an inline keyboard markup with a URL button to open Tonkeeper and a QR code for the connection URL.
+    # Generate a connection URL for the selected wallet
+    generated_url_tonkeeper = await connector.connect(wallets_list[connection[message.text]])
+
+    # Create an inline keyboard markup with a button to open the connection URL
     urlkb = InlineKeyboardMarkup(row_width=1)
-    urlButton = InlineKeyboardButton(text='Open Tonkeeper', url=generated_url_tonkeeper)        
+    urlButton = InlineKeyboardButton(text=f'Open {message.text}', url=generated_url_tonkeeper)        
     urlkb.add(urlButton)
+    
+    # Generate a QR code for the connection URL and save it as an image
     img = qrcode.make(generated_url_tonkeeper)
     path = f'image{random.randint(0, 100000)}.png'
     img.save(path)
     photo = InputFile(path)
-    
-    # Sending the QR code as a photo along with the URL button to open Tonkeeper.
+
+    # Send the QR code image to the user with the inline keyboard markup
     msg = await bot.send_photo(chat_id=message.chat.id, photo=photo, reply_markup=urlkb)
+    # Remove the saved image from the local file system
     os.remove(path)
 
-    # Continuously checking if the connection to Tonkeeper is successful and obtaining the user's wallet address.
-    flag = True
-    while flag:
+    # Check for a successful connection in a loop, with a maximum of 300 iterations (300 seconds)
+    for i in range(300):
         await asyncio.sleep(1)
         if connector.connected:
             if connector.account.address:
-                flag = False
                 address = Address(connector.account.address).to_string(True, True, True)
             break
 
-    # Disconnecting the TonConnect instance after obtaining the user's wallet address.
-    await connector.disconnect()
-    
-    # Deleting the photo message that contained the QR code to keep the chat clean.
+    # Delete the previously sent QR code message
     await msg.delete()
-    
-    # Informing the user that their wallet has been successfully connected and providing a new keyboard.
+
+    # Confirm to the user that the wallet has been successfully connected
     await message.answer('Your wallet has been successfully connected.', reply_markup=kb.Checkkb)
-    
-    # Updating the user's address in the database with the obtained wallet address.
-    cur.execute(f"UPDATE Users SET address = '{address}' WHERE id_tg = {message.from_user.id}")
-    con.commit()
 
-# A message handler function to connect the user's Tonhub wallet to the bot.
-
-@dp.message_handler(text='Tonhub', chat_type=types.ChatType.PRIVATE)
-async def connect_wallet_tonhub(message: types.Message):
-    # Creating a TonConnect instance and restoring the connection to Tonhub using the provided manifest URL.
-    connector = TonConnect(manifest_url='https://raw.githubusercontent.com/AndreyBurnosov/Checking_for_nft_availability/main/pytonconnect-manifest.json')
-    is_connected = await connector.restore_connection()
-    
-    # Getting a list of available wallets from the TonConnect instance.
-    wallets_list = connector.get_wallets()
-
-    # Connecting the user's Tonhub wallet and generating a connection URL.
-    generated_url_tonhub = await connector.connect(wallets_list[1])
-    
-    # Creating an inline keyboard markup with a URL button to open Tonhub and a QR code for the connection URL.
-    urlkb = InlineKeyboardMarkup(row_width=1)
-    urlButton = InlineKeyboardButton(text='Open Tonhub', url=generated_url_tonhub)
-    urlkb.add(urlButton)
-    img = qrcode.make(generated_url_tonhub)
-    path = f'image{random.randint(0, 100000)}.png'
-    img.save(path)
-    photo = InputFile(path)
-    
-    # Sending the QR code as a photo along with the URL button to open Tonhub.
-    msg = await bot.send_photo(chat_id=message.chat.id, photo=photo, reply_markup=urlkb)
-    os.remove(path)
-
-    # Continuously checking if the connection to Tonhub is successful and obtaining the user's wallet address.
-    flag = True
-    while flag:
-        await asyncio.sleep(1)
-        if connector.connected:
-            if connector.account.address:
-                flag = False
-                address = Address(connector.account.address).to_string(True, True, True)
-            break
-
-    # Disconnecting the TonConnect instance after obtaining the user's wallet address.
-    await connector.disconnect()
-    
-    # Deleting the photo message that contained the QR code to keep the chat clean.
-    await msg.delete()
-    
-    # Informing the user that their wallet has been successfully connected and providing a new keyboard.
-    await message.answer('Your wallet has been successfully connected.', reply_markup=kb.Checkkb)
-    
-    # Updating the user's address in the database with the obtained wallet address.
-    cur.execute(f"UPDATE Users SET address = '{address}' WHERE id_tg = {message.from_user.id}")
-    con.commit()
-
-
-@dp.message_handler(chat_type=types.ChatType.PRIVATE)
-async def unknown_command(message: types.Message):
-    await message.answer("unknown command⚠️")
-
-# The main entry point of the Telegram bot application.
-
+# Entry point for the application; starts polling for updates from the Telegram API
 if __name__ == '__main__':
-    # Start polling for updates from the Telegram Bot API using the executor.
-    # The `dp` (Dispatcher) object handles message handling and other event processing.
-    # The `skip_updates=True` parameter tells the executor to skip pending updates when starting.
     executor.start_polling(dp, skip_updates=True)
--- support.py
+++ support.py
@@ -1,10 +1,20 @@
-def build_bd(cur, con):
-    cur.execute('''CREATE TABLE IF NOT EXISTS "Users" (
-        "id"	INTEGER,
-        "id_tg"	INTEGER,
-        "username"	TEXT,
-        "address"	TEXT,
-        PRIMARY KEY("id" AUTOINCREMENT)
-    )''')
+import redis
+from pytonconnect.storage import IStorage
 
-    con.commit()
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+class Storage(IStorage):
+    def __init__(self, id):
+        self.id = id
+    
+    async def set_item(self, key: str, value: str):
+        r.set(key + self.id, value)
+
+    async def get_item(self, key: str, default_value: str = None):
+        if r.exists(key + self.id):
+            return r.get(key + self.id)
+        else:
+            return default_value
+    
+    async def remove_item(self, key: str):
+        r.delete(key + self.id)
